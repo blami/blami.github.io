19a637830dd557b6ba99fdc15417e7b18e9ff5b8
Ondrej Balaz <blami@blami.net>
2021-03-28 23:14:16 +0900

    Progress on css/javascript
    - Fix some rendering glitches.
    - Fix mobile layout misbehavior.
    - Fix spacing between index panels.
    - Add popper.js for nicer popups (currently unused).
    - Add prelim work for pulsing badge (Twitch).

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
-// This adds 'condensed' toggles class on <body> depending on visibility of
-// element with 'navbar-trigger' class. It's used to toggle 'mini-navbar'.
+// Toggle 'condensed' class on <body> depending on visibility of element with
+// 'navbar-trigger' class. It's used to toggle 'mini-navbar'.
 var navbar_trigger = document.querySelector('.navbar-trigger');
 if (navbar_trigger) {
   (new IntersectionObserver(function(e,o){
@@ -10,3 +10,8 @@ if (navbar_trigger) {
     };
   })).observe(navbar_trigger);
 }
+
+// Show pulsing badge on twitch social link icon in non-condensed header. Only
+// makes sense on pages with header.
+// REMOVED as Twitch now requires full authentication and OAuth for that
+
